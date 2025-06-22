a7d2d999a08cb9acbc55fd07175be1a9f9d5758f
sairam-peduri <22b81a67c5@cvr.ac.in>
2025-06-22 19:49:11 +0000

    changed the url for deployment

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,4 @@
-export const BACKEND_URL = "https://lifeline3-1.onrender.com";
+export const BACKEND_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://lifeline3-1.onrender.com" // your Render backend
+    : "http://localhost:5000";           // for local dev
