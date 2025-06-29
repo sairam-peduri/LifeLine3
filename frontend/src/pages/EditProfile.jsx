0d5698d98e26a6fb350fbcdd8bc277743e8ae18f
sairam-peduri <22b81a67c5@cvr.ac.in>
2025-06-29 17:39:18 +0000

    linked

--- frontend/src/pages/EditProfile.jsx
+++ frontend/src/pages/EditProfile.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
 import { useAuth } from "../context/AuthContext";
 
 const EditProfile = () => {
--- frontend/src/pages/ManageAppointments.jsx
+++ frontend/src/pages/ManageAppointments.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
 import { useAuth } from "../context/AuthContext";
 
 const ManageAppointments = () => {
